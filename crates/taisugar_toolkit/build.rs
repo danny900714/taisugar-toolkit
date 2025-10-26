ac3409a5c8f15e9be9b3b46f0d56b38cffd59e8f
Chao Tzu-Hsien <danny900714@gmail.com>
2025-10-26 16:12:43 +0800

    Add the custom build script for windows

---
+++ crates/taisugar_toolkit/build.rs
@@ -0,0 +1,20 @@
+const LANG_CHINESE_TRADITIONAL: u16 = 0x7c04;
+const SUBLANG_CHINESE_TRADITIONAL: u16 = 0x01;
+
+fn make_lang_id(lang: u16, sublang: u16) -> u16 {
+    (sublang << 10) | lang
+}
+
+fn main() {
+    #[cfg(target_os = "windows")]
+    {
+        let mut res = winresource::WindowsResource::new();
+        res.set_icon("resources/windows/app-icon.ico");
+        res.set("ProductName", "台糖工具包");
+        res.set_language(make_lang_id(
+            LANG_CHINESE_TRADITIONAL,
+            SUBLANG_CHINESE_TRADITIONAL,
+        ));
+        res.compile().unwrap();
+    }
+}
